e495c0bc18916d5300f8e365e084f2f369d11641
raul <raul@example.com>
2026-02-13 09:34:31 +0300

    Fixed BenchmarkSettings

--- src/benchmarking-performance/TestPerformance/TestPerformance/BenchmarkSettings.cs
+++ src/benchmarking-performance/TestPerformance/TestPerformance/BenchmarkSettings.cs
@@ -3,7 +3,8 @@ namespace TestPerformance
     public class BenchmarkSettings
     {
         public string InputPath { get; set; } = string.Empty;
-        public int Iterations { get; set; } = 1;
+        public int IterationCount { get; set; } = 1;
+        public int InvocationCount { get; set; } = 1;
         public string? ConnectionString { get; set; }
     }
 }
